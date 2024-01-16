fa8422fdb66b4185a569a38bc80a7f9b627bc956
kavya singampalli <singampallikavya01@gmail.com>
2024-01-16 11:27:51 +0530

    fix: build fixes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Header from "./component/page/header/Header";
 import Page from "./component/page/Page";
-import Content from "./component/page/content/Content";
 
 function App() {
   return (
